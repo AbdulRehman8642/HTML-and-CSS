aa157b49e3cdfff5e3aa1029904e6047708a6b2d
AbdulRehman8642 <abdulrehmankhan8642@gmail.com>
2024-06-13 18:56:03 +0500

    Periodic Table: my first commit

---
+++ practice/sign up login form/app.js
@@ -0,0 +1,9 @@
+function signup(){
+    document.getElementById("signin").style.display = "none";
+    document.getElementById("signup").style.display = "flex";
+}
+
+function signin(){
+    document.getElementById("signin").style.display = "flex";
+    document.getElementById("signup").style.display = "none";
+}
