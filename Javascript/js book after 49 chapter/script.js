11a8bed93585703533048a1c270ae9afdeedabc6
AbdulRehman8642 <abdulrehmankhan8642@gmail.com>
2024-10-09 21:34:12 +0500

    onblur on <p> is not working

--- Javascript/js book after 49 chapter/script.js
+++ Javascript/js book after 49 chapter/script.js
@@ -1,5 +1,80 @@
-function checkAddress(fieldId){
-    if(document.getElementById(fieldId).value === ""){
-        alert("Email field is required")
-    }
+// function checkAddress(fieldId){
+//     if(document.getElementById(fieldId).value === ""){
+//         alert("Email field is required")
+//     }
+// }
+
+// function fillCity(){
+//     var cityValue;
+//     var zipValue = document.getElementById("zip").value;
+//     switch(zipValue){
+//         case "0020" :
+//         cityValue = "Korangi"
+//         break;
+
+//         case "0021" :
+//         cityValue = "Nazimabad"
+//         break;
+
+//         case "0022" :
+//         cityValue = "Gulshan"
+//         break;
+
+//         case "0023" :
+//         cityValue = "Clifton"
+//         break;
+
+//         case "0024" :
+//         cityValue = "Bahria Town"
+//         break;
+
+//         case "0025" :
+//         cityValue = "PowerHouse"
+//         break;
+
+//         case "0026" :
+//         cityValue = "Bahadurabad"
+//         break;
+//     }
+//     console.log(cityValue)
+//     // console.log(document.getElementById("city").value)
+//     document.getElementById("city").value = cityValue
+// }
+
+// function expandMore(){
+//     var expParagraph = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur nemo ad perferendis neque optio quo porro maxime id quia ipsa, molestias sint illo sapiente officia inventore, laboriosam eum. Voluptate, reprehenderit?"
+
+//     document.getElementById("para").innerHTML = expParagraph
+// }
+
+
+
+// function checkPara(){
+//     var realPara = document.getElementById("whatPara").innerHTML
+//     console.log(realPara)
+// }
+
+
+// var hiddenClass = " hidden"
+// function remove(){
+//     document.getElementById("para").className = hiddenClass
+// }
+
+// var beforePara
+// function swapPic(id, afterPara){
+//     beforePara = document.getElementById(id).innerHTML
+//     document.getElementById(id).innerHTML = afterPara
+// }
+
+// function blurred(id){
+//     console.log(beforePara)
+// }
+
+
+function over(){
+    console.log("First")
+}
+
+function blur(){
+    console.log("Second")
 }
