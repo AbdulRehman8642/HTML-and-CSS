b9fa40402cfb3a29112433e74ef6921f8b0d2c04
AbdulRehman8642 <abdulrehmankhan8642@gmail.com>
2025-01-13 22:07:29 +0500

    react started nvm installed 13-1-25

---
+++ react/13-1-25/App.js
@@ -0,0 +1,8 @@
+import MainHeading from "./MainHeading.js"
+ 
+export default function App(){
+    return <>
+    <h1>Welcome to the application</h1>
+    <MainHeading/>
+    </>
+}
---
+++ react/13-1-25/MainHeading.js
@@ -0,0 +1,12 @@
+// export default function firstComponent (){
+//     const heading1 = {
+//         <h1>hello</h1>
+//     };
+// }
+
+export default function MainHeading() {
+    return(
+        <h1>hello</h1>
+    )
+}
+
