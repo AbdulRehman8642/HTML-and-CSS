46815218c78996a7908dec147f730bfa782cb29d
AbdulRehman8642 <abdulrehmankhan8642@gmail.com>
2025-01-14 21:59:06 +0500

    react: node try to install 14-1-25

--- react/13-1-25/MainHeading.js
+++ react/13-1-25/MainHeading.js
@@ -1,9 +1,3 @@
-// export default function firstComponent (){
-//     const heading1 = {
-//         <h1>hello</h1>
-//     };
-// }
-
 export default function MainHeading() {
     return(
         <h1>hello</h1>
