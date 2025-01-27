e30abcb65d91ba5f02aed8f40ff55bd71e1046ce
AbdulRehman8642 <abdulrehmankhan8642@gmail.com>
2025-01-27 22:05:31 +0500

    react router installed 27-1-25

--- react/react app 2/my-app/src/App.jsx
+++ react/react app 2/my-app/src/App.jsx
@@ -3,12 +3,24 @@ import Header from "./page layout/header/header.jsx"
 import Body from "./page layout/body/body.jsx"
 import Footer from './page layout/footer/footer.jsx'
 import Counter from './page layout/counter/counter.jsx'
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
 function App() {
 
   return (
     <>
-     
+      {/* <Router>
+        <Switch>
+            <Route path="./page layout/header/header.jsx">
+              <Header />
+            </Route>
+        </Switch>
+      </Router> */}
       {/* <TestComponent /> */}
       <Header />
       <Body />
