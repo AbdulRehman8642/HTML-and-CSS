82014a974ee106b27732bbb8b6d9dff41f9a4a2a
AbdulRehman8642 <abdulrehmankhan8642@gmail.com>
2025-07-25 17:09:00 +0500

    todo project (25 july)

---
+++ react and firebase/todo app/client/src/components/NewTaskPopup.jsx
@@ -0,0 +1,36 @@
+import { React, useState } from "react";
+
+export default function NewTaskPopup() {
+  return (
+    <>
+      <div className="taskFormParent">
+        <div className="subjectParent">
+          <p>Task Subject</p>
+          <input
+            type="text"
+            id="subjectInpId"
+            placeholder="Enter Task Subject"
+          />
+        </div>
+        <div className="textareaParent">
+          <p>Task Description</p>
+          <textarea
+            id="descriptionInpId"
+            placeholder="Enter Task description"
+          ></textarea>
+        </div>
+        <div className="taskDueDateParent">
+          <p>Task Due Date</p>
+          <input type="date" id="taskDDateId" />
+        </div>
+        <div className="taskStatusParent">
+          <p>Task Status</p>
+          <input type="checkbox" name="started" id="startedStatusId" />
+          <label htmlFor="startedStatusId">Started</label>
+          <input type="checkbox" name="not-started" id="notStartedStatusId" />
+          <label htmlFor="notStartedStatusId">Not started</label>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ react and firebase/todo app/client/src/components/parent.jsx
@@ -0,0 +1,43 @@
+import { React, useState } from "react";
+import "../App.css";
+import NewTaskPopup from "./NewTaskPopup";
+
+export default function Parent() {
+  const [taskPopupReq, setTaskPopupReq] = useState(false);
+
+  const taskPopupFun = () => {
+    // setTaskPopupReq(true)
+    if(taskPopupFun === true){
+        console.log("it is true")
+    }
+  };
+
+
+  return (
+    <>
+      <div className="parent">
+        <nav className="navbar">
+          <div className="navItemsParent">
+            <div className="allTaskNav navItem">All Tasks</div>
+            <div className="compTaskNav navItem">Completed Tasks</div>
+            <div className="remainTaskNav navItem">Remaining Tasks</div>
+          </div>
+          <div className="newTaskParent">
+            <button onClick={taskPopupFun} className="newTaskBtn">New</button>
+          </div>
+        </nav>
+
+        <div className="bodyParent">
+          <div className="taskCardsParent">
+            <div className="taskCard">
+              <div className="subjectDiv">
+                <h4 className="subject">Task 1</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <NewTaskPopup />
+    </>
+  );
+}
